16144f387d14fd82a4ba593e8874ba0e44741680
kdw <zkdlwu94@naver.com>
2021-10-18 21:03:25 +0900

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import apikey from '../keys'
+const apiKey = apikey;
+
+
+const App = () => {
+    return (
+        <div className="app__wrapper">
+           
+        </div>
+    )
+}
+
+export default App

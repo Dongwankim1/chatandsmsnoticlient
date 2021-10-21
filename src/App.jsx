6d974b38546dc47f2347228c150dd21dd2e6a1e6
kdw <zkdlwu94@naver.com>
2021-10-21 23:09:05 +0900

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import {StreamChat} from 'stream-chat';
-import {Chat} from 'stream-chat-react';
+import {ChannelList, Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import apikey from '../keys'
+import ChannelListContainer from './components/ChannelListContainer'
+import ChannelContainer from './components/ChannelContainer';
 const apiKey = apikey;
+
+const client = StreamChat.getInstance(apiKey);
 
 
 const App = () => {
     return (
         <div className="app__wrapper">
-           
+           <Chat client={client} theme="team light">
+               <ChannelListContainer/>
+
+                <ChannelContainer/>
+           </Chat>
         </div>
     )
 }
